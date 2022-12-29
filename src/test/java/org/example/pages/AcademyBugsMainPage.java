061ecc8d049b7d816778677ed7a7647acd0aaad5
&mazen <mazenkhairy200@gmail.com>
2022-12-30 00:04:12 +0200

    commit1

---
+++ src/test/java/org/example/pages/AcademyBugsMainPage.java
@@ -0,0 +1,79 @@
+package org.example.pages;
+
+import org.example.stepDefinitions.Hooks;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class AcademyBugsMainPage {
+
+
+    //Buttons
+    public WebElement addtocartbtn0(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[1]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn1(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[2]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn2(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[3]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn3(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[4]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn4(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[5]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn5(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[6]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn6(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[7]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement selectoptionsbtn0(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[8]/div/div[3]/div[2]/span/a"));
+    }
+    public WebElement addtocartbtn7(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[9]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn8(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[10]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement loginforpricingbtn(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[11]/div/div[3]/div[1]/span/a"));
+    }
+    public WebElement addtocartbtn9(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[12]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn10(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[13]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement selectoptions1(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[14]/div/div[3]/div[2]/span/a"));
+    }
+    public WebElement addtocartbtn11(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[15]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement selectoptions2(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[16]/div/div[3]/div[2]/span/a"));
+    }
+    public WebElement addtocartbtn12(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[17]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement addtocartbtn13(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[18]/div/div[3]/div[2]/span/a[1]"));
+    }
+    //Other Buttons
+    public WebElement sortviewby10(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/ul/li[18]/div/div[3]/div[2]/span/a[1]"));
+    }
+    public WebElement sortviewby25(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/div[1]/span[1]/a[2]"));
+    }
+    public WebElement sortviewby50(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/div[1]/span[1]/a[2]"));
+    }
+    public WebElement sortingselection(){
+        return Hooks.driver.findElement(By.xpath("/html/body/div[3]/div/div/div/main/article/div/section/div[1]/select"));
+    }
+
+}
